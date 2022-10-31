5b683dfd58c26714f77e1978a451bf8cc5f95963
aginga <brianaginga35@gmail.com>
2022-10-31 17:15:28 +0300

    new code

---
+++ bool.js
@@ -0,0 +1,22 @@
+// by using of comparison operator
+
+console.log(3 < 8);
+console.log(5 > 3);
+
+//you can also compare string
+console.log('brian' < 'aginga');
+
+// not equal to
+
+console.log(8 != 5);
+
+// equal to
+
+console.log( 9 == 9);
+
+let a = 5,  n = 4;
+
+console.log(a + n);
+
+
+
