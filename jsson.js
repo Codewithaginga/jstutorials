635bc1336643c549eca50094e642d7ad396a1471
aginga <brianaginga35@gmail.com>
2022-11-05 15:44:32 +0300

    new

---
+++ jsson.js
@@ -0,0 +1,24 @@
+const Epl = [
+    {
+        club: 'Arsenal',
+        stadium: 'Emirates',
+        position: 1
+
+    },
+    {
+        club: 'Manchester city',
+        stadium: 'Etihard',
+        position: 2
+
+    },
+    {
+        club: 'Newcastle united',
+        stadium: 'st Marys',
+        position: 3
+
+    },
+
+]
+
+const toJSON = JSON.stringify(Epl);
+console.log(toJSON);
