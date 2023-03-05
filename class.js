91c69e10040cda87b8ae5922141082f2fdf07a05
aginga <brianaginga35@gmail.com>
2023-03-05 15:21:03 -0800

    oop

--- class.js
+++ class.js
@@ -1,11 +1,28 @@
-function Audi(wheel, make) {
-    this.wheel = wheel
-    this.make = make
+// function Audi(wheel, make) {
+//     this.wheel = wheel
+//     this.make = make
 
-    this.display = function() {
-        return `${this.wheel} ${this.make}`;
+//     this.display = function() {
+//         return `${this.wheel} ${this.make}`;
+//     }
+// }
+
+// let audu = new Audi(4, 'salon car');
+// console.log(audu);
+
+
+//OOP
+
+let purchase = {
+    benz: 1233344,
+    texPay: 12,
+
+    totalPay: function () {
+        let calc = purchase.benz * purchase.texPay;
+        console.log('Total Price is ', calc);
+
     }
 }
 
-let audu = new Audi(4, 'salon car');
-console.log(audu);
+purchase.totalPay();
+
